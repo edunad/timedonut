fb14c22f3645c86c67bf3f4ecf643826f696b891
failcake <failcake.work@gmail.com>
2018-11-06 21:20:14 +0000

    Initial commit

---
+++ Assets/Scripts/Ingame/Entities/logic_target.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class logic_target : MonoBehaviour {
+	private BoxCollider2D _boxCollider;
+
+	// Use this for initialization
+	public void Start () {
+		this.name = "logic_target";
+
+		_boxCollider = GetComponent<BoxCollider2D>();
+		_boxCollider.isTrigger = true;
+
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		
+	}
+
+	public void OnDrawGizmos() {
+		Gizmos.color = new Color(255, 0, 0, 100);
+
+		// Draw PIT
+		Vector3 col = transform.position;
+
+		// Draw Icon
+		Gizmos.DrawIcon(new Vector3(col.x, col.y, col.z), "logic_dead", true);
+	}
+}
